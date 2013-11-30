2a34ff2f6e4fff1bab9fbfbdc9e028a856736a52
Sam Bolgert <sam@livelovely.com>
2013-11-29 18:54:34 -0800

    adding filesplitter

---
+++ src/FileSplitter.java
@@ -0,0 +1,18 @@
+/**
+ * Author: Sam Bolgert
+ * Date: 11/29/13
+ * Time: 6:52 PM
+ */
+public class FileSplitter {
+
+
+    public static void main(String args[]){
+        //read the file
+
+        //run the //article xpath
+
+        //for article in articles
+            //if count >= 1000
+                //write file
+    }
+}
